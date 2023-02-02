af911f4ee8702e7003060de6d8409031a7bd3422
ber <ber@berdeMacBook-Pro.local>
2023-02-02 09:56:09 -0800

    view all information is done

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,11 +9,24 @@ VALUES
 
 INSERT INTO department_role (title, salary, department_id) 
 VALUES 
-("Sales Lead, 500,1"),
-("Salesperson, 100,1"),
-("Lead Engineer, 300,2"),
-("Software Engineer, 200, 2"),
-("Account Manager, 600, 4"),
-("Accountant, 300, 4"),
-("Legal Team Lead, 800, 3"),
-("Lawyer, 600, 3")
+("Sales Lead", 500,1),
+("Salesperson", 100,1),
+("Lead Engineer", 300,2),
+("Software Engineer", 200, 2),
+("Account Manager", 600, 4),
+("Accountant", 300, 4),
+("Legal Team Lead", 800, 3),
+("Lawyer", 600, 3);
+
+INSERT INTO employee (first_name, last_name,role_id, manager_id) 
+VALUES 
+("Jun","Zhang",1,NULL),
+("Mei","Wang",2,1),
+("Jian","Liu",3,NULL ),
+("Lily","Chen",4,3),
+("Hui","Lee",5,NULL),
+("Sisi","Jin",6,5),
+("Jessy","Cooper",7,NULL),
+("June","Lin",8,7)
+
+
--- index.js
+++ index.js
@@ -1,15 +1,57 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const mysql = require("mysql2");
 
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'action',
-      choices: ['View All Employees', 'Add Employee', 'Update Employee Role','View All Roles','Add Role','View All Dpartment','Add Department']
-    },
-  ])
-  .then((data) => {
-console.log(data.action)
-  });
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "company_employee_db",
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
+function prompt_employee() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "action",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Department",
+          "Add Department",
+        ],
+      },
+    ])
+    .then((data) => {
+      if (data.action == "View All Department") {
+        db.query("SELECT * FROM department", function (err, results) {
+          console.table(results);
+        });
+      } else if (data.action == "View All Roles") {
+        db.query("SELECT * FROM department_role", function (err, results) {
+          console.table(results);
+        });
+      } else if (data.action == "View All Employees") {
+        db.query(
+          "SELECT employee.id, employee.first_name, employee.last_name, department_role.title, department_role.department_id, department_role.salary FROM employee JOIN department_role ON employee.role_id = department_role.department_id",
+          function (err, results) {
+            console.table(results);
+          }
+        );
+      } else if (data.action == "Add Employee") {
+        console.log(data);
+      } else {
+        console.log(data);
+      }
+    //   prompt_employee();
+    });
+}
+
+prompt_employee();
